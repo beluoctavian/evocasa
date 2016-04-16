ed5f719b864b26cb528626c988cc9f35873c9611
calimanleontin <calimanleontin@gmail.com>
2016-04-16 20:30:58 +0300

    owner migration

---
+++ docroot/database/migrations/2016_04_16_203033_owner.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Owner extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('owner', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('first_name');
+			$table->string('last_name');
+			$table->string('phone');
+			$table->string('email');
+			$table->string('cadaster');
+			$table->string('registrations');
+			$table->string('energy_certificate');
+			$table->string('urbanism_certificate');
+			$table->boolean('map_pictures');
+			$table->boolean('rehabilitated_block');
+			$table->string('address');
+			$table->integer('advert_id');
+			$table->foreign('advert_id')
+				->references('id')
+				->on('advert')
+				->onDelete('cascade');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}

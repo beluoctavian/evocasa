f7cbad39523dac3403e17e8eb022b624d5bd2953
calimanleontin <calimanleontin@gmail.com>
2016-04-16 22:22:49 +0300

    advert model

---
+++ docroot/app/Advert.php
@@ -0,0 +1,50 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Advert extends Model {
+
+	protected $guarded = [];
+
+    protected $table = 'advert';
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function owner()
+    {
+        return $this->hasOne('App\Owner');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function observations()
+    {
+        return $this->hasMany('App\Observation');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function apartment()
+    {
+        return $this->hasOne('App\Apartment');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function house()
+    {
+        return $this->hasOne('App\House');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function terrain()
+    {
+        return $this->hasOne('App\Terrain');
+    }
+}

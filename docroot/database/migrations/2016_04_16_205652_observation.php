611da09fbc9fb1b5960bd74d67e0c767e1863a47
calimanleontin <calimanleontin@gmail.com>
2016-04-16 21:03:31 +0300

    observation migration

---
+++ docroot/database/migrations/2016_04_16_205652_observation.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Observation extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('observation', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('text');
+			$table->integer('owner_id');
+			$table->foreign('owner_id')
+				->references('id')
+				->on('owner')
+				->onDelete('cascade');
+			$table->timestamps();
+
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('observation', function(Blueprint $table)
+		{
+			//
+		});
+	}
+
+}

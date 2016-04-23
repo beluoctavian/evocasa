ede778aeca5170b44c8a1c6b856c0b63db633bd2
Belu Octavian <belu.octavian@yahoo.com>
2016-04-23 21:59:09 +0300

    House can be created now

--- docroot/app/House.php
+++ docroot/app/House.php
@@ -8,12 +8,68 @@ class House extends Model {
 
     protected $table = 'house';
 
+    protected $fillable = [
+      'land_area',
+      'street_opening',
+      'footprint',
+      'total_area',
+      'level_area',
+      'height',
+      'built_year',
+      'bathrooms',
+      'obs_bathrooms',
+      'sanitary',
+      'obs_sanitary',
+      'balconies',
+      'obs_balconies',
+      'garage',
+      'obs_garage',
+      'advert_id',
+    ];
+
+    public static $properties = [
+        'land_area',
+        'street_opening',
+        'footprint',
+        'total_area',
+        'level_area',
+        'height',
+        'built_year',
+        'bathrooms',
+        'obs_bathrooms',
+        'sanitary',
+        'obs_sanitary',
+        'balconies',
+        'obs_balconies',
+        'garage',
+        'obs_garage',
+        'advert_id',
+    ];
+
     /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
     public function advert()
     {
         return $this->belongsTo('App\Advert');
+    }
+
+    /**
+     * @param array $parameters
+     * @param \App\Advert $advert
+     * @return static
+     */
+    public static function createFromArray(array $parameters, Advert $advert)
+    {
+        $valid_parameters = [
+          'advert_id' => $advert->id,
+        ];
+        foreach ($parameters as $key => $value) {
+            if (in_array($key, self::$properties)) {
+                $valid_parameters[$key] = $value;
+            }
+        }
+        return self::create($valid_parameters);
     }
 
 }

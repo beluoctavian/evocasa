60086d4a95a19d093fa0180683fbcf9dd7de0066
Belu Octavian <belu.octavian@yahoo.com>
2016-04-23 22:00:17 +0300

    Terrain can now be created'

--- docroot/app/Terrain.php
+++ docroot/app/Terrain.php
@@ -8,12 +8,46 @@ class Terrain extends Model {
 
     protected $table = 'terrain';
 
+    protected $fillable = [
+      'total_area',
+      'street_opening',
+      'depth',
+      'access_width',
+      'advert_id',
+    ];
+
+    public static $properties = [
+      'total_area',
+      'street_opening',
+      'depth',
+      'access_width',
+      'advert_id',
+    ];
+
     /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
     public function advert()
     {
         return $this->belongsTo('App\Advert');
+    }
+
+    /**
+     * @param array $parameters
+     * @param \App\Advert $advert
+     * @return static
+     */
+    public static function createFromArray(array $parameters, Advert $advert)
+    {
+        $valid_parameters = [
+          'advert_id' => $advert->id,
+        ];
+        foreach ($parameters as $key => $value) {
+            if (in_array($key, self::$properties)) {
+                $valid_parameters[$key] = $value;
+            }
+        }
+        return self::create($valid_parameters);
     }
 
 }

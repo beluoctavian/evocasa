a1e1e1bfc4e9b7d1b2d6c4f145a2d43168ab55e9
Belu Octavian <belu.octavian@yahoo.com>
2016-05-07 16:39:51 +0300

    Fixed new urls

--- docroot/resources/views/pages/new_index.blade.php
+++ docroot/resources/views/pages/new_index.blade.php
@@ -52,11 +52,11 @@
                                         <form method="POST" action="{{ URL::to('sterge-anunt') }}" onSubmit="return confirm('Sigur vrei sa stergi anuntul?');">
                                             <input type="hidden" name="_token" value="{{ csrf_token() }}">
                                             <input type="hidden" name="id" value="{{ $advert['id'] }}">
-                                            <a href="{{ URL::to('editeaza-anunt/' . $advert['id']) }}" class="btn btn-primary"><i class="fa fa-pencil"></i></a>
+                                            <a href="{{ URL::to('advert/edit/' . $advert['id']) }}" class="btn btn-primary"><i class="fa fa-pencil"></i></a>
                                             <br>
-                                            <a href="{{ URL::to('upload-images/' . $advert['id']) }}" class="btn btn-success"><i class="fa fa-file-image-o"></i></a>
+                                            <a href="{{ URL::to('advert/images/' . $advert['id']) }}" class="btn btn-success"><i class="fa fa-file-image-o"></i></a>
                                             <br>
-                                            <a href="{{ URL::to('update-date/' . $advert['id']) }}" class="btn btn-warning"><i class="fa fa-wrench"></i></a>
+                                            <a href="{{ URL::to('advert/update/' . $advert['id']) }}" class="btn btn-warning"><i class="fa fa-wrench"></i></a>
                                             <br>
                                             <button type="submit" class="btn btn-danger"><i class="fa fa-times"></i></button>
                                         </form>
@@ -66,7 +66,7 @@
                                     @if(Auth::guest())
                                         <a href="{{ URL::to('anunturi/' . $advert['id']) }}">
                                             @else
-                                                <a href="{{ URL::to('editeaza-anunt/' . $advert['id']) }}">
+                                                <a href="{{ URL::to('advert/edit/' . $advert['id']) }}">
                                                     @endif
                                                     @if(File::exists('uploaded-images/anunt_' . $advert['id'] . '/'))
                                                         <?php $files = File::allFiles('uploaded-images/anunt_' . $advert['id'] . '/'); sort($files); ?>
@@ -89,13 +89,13 @@
                                         @if(!empty($item['advert_status']))
                                             @foreach($item['advert_status'] as $status)
                                                 @if(strpos($status['title'],'Inactiv') === false)
-                                                    <h2><a class="" href="{{ URL::to('editeaza-anunt/' . $advert['id']) }}">{{ $advert['title'] }}</a></h2>
+                                                    <h2><a class="" href="{{ URL::to('advert/edit/' . $advert['id']) }}">{{ $advert['title'] }}</a></h2>
                                                 @else
-                                                    <h2><a class="red" href="{{ URL::to('editeaza-anunt/' . $advert['id']) }}">{{ $advert['title'] }}</a></h2>
+                                                    <h2><a class="red" href="{{ URL::to('advert/edit/' . $advert['id']) }}">{{ $advert['title'] }}</a></h2>
                                                 @endif
                                                 @endforeach
                                         @else
-                                            <h2><a class="" href="{{ URL::to('editeaza-anunt/' . $advert['id']) }}">{{ $advert['title'] }}</a></h2>
+                                            <h2><a class="" href="{{ URL::to('advert/edit/' . $advert['id']) }}">{{ $advert['title'] }}</a></h2>
                                         @endif
                                     @endif
                                     <div class="price">

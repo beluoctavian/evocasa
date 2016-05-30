210945bf1558441974a7c778105eeeb5a3e4e19d
Belu Octavian <belu.octavian@yahoo.com>
2016-05-30 19:38:29 +0300

    Create default users

--- docroot/database/migrations/2015_06_02_095130_CreateUsersTable.php
+++ docroot/database/migrations/2015_06_02_095130_CreateUsersTable.php
@@ -22,13 +22,38 @@ class CreateUsersTable extends Migration {
             $table->timestamps();
         });
 
-        $user = new App\User();
-        $user->username = 'administrator';
-        $user->name = 'administrator';
-        $user->email = 'administrator@example.com';
-        $user->code = 'AD';
-        $user->password = Hash::make('administrator');
-        $user->save();
+        $users = [
+            [
+                'username' => 'ysaccosmin@evocasainvest.ro',
+                'name' => 'Ionescu Isac Cosmin',
+                'email' => 'cosmin@evocasainvest.ro',
+                'password' => '$2y$10$FMAifdEoPNlpuMFti38Yy.vWvMjPKj6ZoMJ6AjtuYuEZARYAgvwIW',
+                'code' => 'IC',
+            ],
+            [
+                'username' => 'a.gabriela@evocasainvest.ro',
+                'name' => 'Agrapinei Gabriela',
+                'email' => 'gabriela@evocasainvest.ro',
+                'password' => '$2y$10$s7qwu/P5cnb1BE4X8YMwKu0Rehe/hByPc9Mk3oKnBh8T3UuNhhEca',
+                'code' => 'AG',
+            ],
+            [
+                'username' => 'administrator',
+                'name' => 'administrator',
+                'email' => 'administrator@evocasainvest.ro',
+                'password' => '$2y$10$VQFqI47HvqMXpQBl0RjYSOkVzahyqYnbZ5YGJ7dVGBFjUI/TJKDpO',
+                'code' => 'ADMIN',
+            ],
+        ];
+        foreach ($users as $user) {
+            $newUser = new App\User();
+            $newUser->username = $user['username'];
+            $newUser->name = $user['name'];
+            $newUser->email = $user['email'];
+            $newUser->code = $user['code'];
+            $newUser->password = $user['password'];
+            $newUser->save();
+        }
 
     }
 
